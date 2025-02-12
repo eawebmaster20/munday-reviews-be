990aa6221d2280970d10f1d92cc8e2562eb9b2bc
eawebmaster20 <eaweb@gmail.com>
2025-02-12 20:14:07 +0000

    feat: update backend setup with environment configuration and switch database to mysql

--- models/index.js
+++ models/index.js
@@ -1,29 +1,18 @@
-// const { Sequelize } = require('sequelize');
-// const sequelize = new Sequelize({
-//   dialect: 'sqlite',
-//   storage: './database.sqlite'
-// });
-
-// const User = require('./user')(sequelize);
-// const Company = require('./company')(sequelize);
-// const Review = require('./review')(sequelize);
-
-
-// module.exports = { sequelize, User, Company, Review };
 
 
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './database.sqlite',
-  logging: false,  // Optional: Disable logging for cleaner output
+const dotenv = require('dotenv');
+
+dotenv.config();
+const sequelize = new Sequelize(process.env.DB_URL,{
+  dialect: 'mysql',
+  logging: true, 
 });
 
 const User = require('./user')(sequelize, Sequelize.DataTypes);
 const Company = require('./company')(sequelize, Sequelize.DataTypes);
 const Review = require('./review')(sequelize, Sequelize.DataTypes);
 
-// Initialize associations
 const models = { User, Company, Review };
 
 Object.keys(models).forEach((modelName) => {
